def372bac82e64058d68948db3cfd0b0958ada2e
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-02-22 06:55:51 -0300

    feat: add auth modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { NavComponent } from './nav/nav.component';
 import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, UserModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  @Input() modalId: string = '';
 
+  constructor(private elementRef: ElementRef, public modal: ModalService) {}
+
+  ngOnInit(): void {
+    document.body.appendChild(this.elementRef.nativeElement);
+  }
+
+  closeModal(): void {
+    this.modal.closeModal(this.modalId);
+  }
 }
