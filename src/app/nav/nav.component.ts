5f93c2f084a6b5de337986f38e836c48656ea1c6
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-02-24 08:52:00 -0300

    feat: modal tabs

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 
 @Component({
--- src/app/shared/tab-container/tab-container.component.ts
+++ src/app/shared/tab-container/tab-container.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tab-container',
   templateUrl: './tab-container.component.html',
-  styleUrls: ['./tab-container.component.css']
+  styleUrls: ['./tab-container.component.css'],
 })
 export class TabContainerComponent {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
 
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent): boolean {
+    this.tabs?.forEach((tab) => (tab.active = false));
+    tab.active = true;
+    return false;
+  }
 }
