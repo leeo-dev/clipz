1ced3f92d67f90432f239c0d38be1f6f327fc727
Leonardo <leeodesign@hotmail.com>
2023-03-03 08:49:24 -0300

    chore: create register form component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AuthModalComponent, RegisterComponent],
+  declarations: [AuthModalComponent, RegisterComponent, LoginComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
