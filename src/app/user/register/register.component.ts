096fd7b78cf08def42837d291fc0a8751f3fe360
Leonardo <leeodesign@hotmail.com>
2023-03-03 09:19:28 -0300

    feat: add validator in necessary codes

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorHelper } from '../../../helpers/validator-helper';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +8,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  name: FormControl = new FormControl('', [Validators.required]);
-  email: FormControl = new FormControl('', [Validators.required]);
-  age: FormControl = new FormControl('', [Validators.required]);
+  name: FormControl = new FormControl('', [
+    Validators.required,
+    ...ValidatorHelper.minMaxLength(),
+  ]);
+  email: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  age: FormControl = new FormControl('', [
+    Validators.required,
+    ...ValidatorHelper.minMax(18, 80),
+  ]);
   password: FormControl = new FormControl('', [Validators.required]);
-  confirmPassword: FormControl = new FormControl('', [Validators.required]);
-  phoneNumber: FormControl = new FormControl('', [Validators.required]);
+  confirmPassword: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+  phoneNumber: FormControl = new FormControl('', [
+    Validators.required,
+    ...ValidatorHelper.minMaxLength(11, 11),
+  ]);
 
   registerForm: FormGroup = new FormGroup({
     name: this.name,
--- src/helpers/validator-helper.ts
+++ src/helpers/validator-helper.ts
@@ -1,7 +1,11 @@
 import { ValidatorFn, Validators } from '@angular/forms';
 
-class ValidatorHelper {
-  static minMax(min: number = 3, max: number = 25): ValidatorFn[] {
+export class ValidatorHelper {
+  static minMaxLength(min: number = 3, max: number = 25): ValidatorFn[] {
     return [Validators.minLength(min), Validators.maxLength(max)];
+  }
+
+  static minMax(min: number = 3, max: number = 25): ValidatorFn[] {
+    return [Validators.min(min), Validators.max(max)];
   }
 }
